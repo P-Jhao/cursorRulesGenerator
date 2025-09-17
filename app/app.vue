90eb3f17e5b463fdd4349d268cbf3ee03c978e0f
P-Jhao <2212688637@qq.com>
2025-09-17 14:05:48 +0800

    v1.4.4 新增了seoMeta

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,9 @@
-<script>
-// useSeoMeta({
-//   title: 'Cursor Rules 生成器',
-//   description: '一个基于 Nuxt 3 的自动生成 Cursor Rules 的网页应用，支持用户注册登录和历史记录管理。',
+<script setup lang="ts">
 
-// })
+useSeoMeta({
+  title: 'cursor rules 生成器',
+  description: '一个基于 Nuxt 3 的自动生成 Cursor Rules 的网页应用，支持用户注册登录和历史记录管理。',
+})
 </script>
 
 <template>
